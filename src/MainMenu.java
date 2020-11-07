ff2c1d06ec2233c08d72e03a322205d4b182ffc7
abd345 <69235728+abd345@users.noreply.github.com>
2020-11-07 13:15:55 +0200

    version 0.35 and tests

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -6,15 +6,14 @@ public class MainMenu {
         final String MENU_TEXT="R: Recite poem, Q: Quit";
         String[] poem= new String[]{"Near the road it flowered," ,"the mallow—and by my horse",  "has been devoured!",};
         String author="Matsuo Basho(1684), Rendition by Harold G. Henderson";
-        String ans=new String();
+        String ans="";
+        Recite r=new Recite(poem);
         while (!ans.equals("Q"))
         {
-
             ans=JOptionPane.showInputDialog(fullpoem(poem)+"-"+author+"\n"+MENU_TEXT);
-            switch (ans){
-                case "R":
+            if(ans.equals("R")){
+               String[] s=JOptionPane.showInputDialog("Enter Verses to recite separated by spaces").split(" ");
 
-                    break;
             }
         }
     }
